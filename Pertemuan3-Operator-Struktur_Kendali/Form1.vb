e83ca1953255f560f3c97fcd1b8b40767f225d79
Selo frisilia <seloselofrisiliasirait@gmail.com>
2026-09-02 16:02:45 +0700

    Pertemuan 3

---
+++ Pertemuan3-Operator-Struktur_Kendali/Form1.vb
@@ -0,0 +1,37 @@
+﻿Public Class Form1
+    Private Sub txtNilai_TextChanged(sender As Object, e As EventArgs) Handles txtNilai.TextChanged
+
+    End Sub
+
+    Private Sub btnInput_Click(sender As Object, e As EventArgs) Handles btnInput.Click
+        Dim nilaiUjian As Integer
+        If Not Integer.TryParse(txtNilai.Text, nilaiUjian) Then
+            MessageBox.Show("Masukkan Angka")
+            txtNilai.Focus()
+
+        End If
+
+        If nilaiUjian < 0 OrElse nilaiUjian > 100 Then
+            MessageBox.Show("Masukkan nilai 0 - 100")
+        End If
+
+        If nilaiUjian <= 50 Then
+            picImage.Image = Image.FromFile("Assets\Asset1.gif")
+        ElseIf nilaiUjian <= 75 Then
+            picImage.Image = Image.FromFile("Assets\Asset1.gif")
+        Else
+            picImage.Image = Image.FromFile("Assets\Asset1.gif")
+        End If
+
+    End Sub
+
+    Private Sub txtNilai_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtNilai.KeyPress
+        If Not Char.IsControl(e.KeyChar) AndAlso Not Char.IsDigit(e.KeyChar) Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub picImage_Click(sender As Object, e As EventArgs) Handles picImage.Click
+
+    End Sub
+End Class
